a01cab5699fa3da4f998c88292d7f34ba37ae4c5
Brilian Anugra <48582219+Brilian-Pro@users.noreply.github.com>
2020-07-12 12:41:10 +0700

    Add files via upload

---
+++ responsi/daftar.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Pemrograman WEB 12</title>
+    <link rel="stylesheet" type="text/css" href="css.css">
+</head>
+<body>
+    <h1>Menu pendaftaran : Tim 2n3 project</h1>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="daftar.php">Order</a></li>
+        <li><a href="biodata.php">Tentang</a></li>
+    </ul>
+
+    <form action="" method="post">
+        <table class="rekuitmen">
+            <tr>
+                <th>Rekuitmen anggota Tim Programmer</th>
+            </tr>
+            <tr>
+                <td>Nama Lengkap : </td>
+            </tr>
+            <tr>
+                <td><input type="text" name="nama"></td>
+            </tr>
+            <tr>
+                <td>Jenis Kelamin</td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="radio" name="gender" value="Laki-laki"> Laki- laki
+                    <input type="radio" name="gender" value="Perempuan">Perempuan
+                </td>
+            </tr>
+            <tr>
+                <td>Email : </td>
+            </tr>
+            <tr>
+                <td><input type="text" name="email"></td>
+            </tr>
+            <tr>
+                <td>Nomor WA/HP : </td>
+            </tr>
+            <tr>
+                <td><input type="number" name="nomor"></td>
+            </tr>
+            <tr>
+                <td>Agama :</td>
+            </tr>
+            <tr>
+                <td>
+                    <select name="agama" >
+                        <option>Pilih</option>
+                        <option value="Islam">Islam</option>
+                        <option value="Kristen">Kristen</option>
+                        <option value="Hindu">Hindu</option>
+                        <option value="Buddha">Buddha</option>
+                        <option value="Konghuchu">Konghuchu</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>Bahasa yang dikuasai :</td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="checkbox" name="bahasa[]" value="HTML">HTML<br>
+                    <input type="checkbox" name="bahasa[]" value="CSS">CSS<br>
+                    <input type="checkbox" name="bahasa[]" value="Javascript">Javascript<br>
+                    <input type="checkbox" name="bahasa[]" value="Phyton">Phyton<br>
+                    <input type="checkbox" name="bahasa[]" value="Java">Java<br>
+                    <input type="checkbox" name="bahasa[]" value="C++">C++   <br>
+                </td>
+            </tr>
+            <tr>
+                <td>Deskripsi Diri :</td>
+            </tr>
+            <tr>
+                <td><textarea name="gambaran" rows="10" cols="25"></textarea></td>
+            </tr>
+            <tr>
+                <td><input type="submit" name="kirim" value="Kirim" class="tombol">       <input type="reset" name="reset" class="tombol"></td>
+            </tr>
+        </table>
+    </form>
+
+    <?php
+
+        if(isset($_POST['kirim'])){
+            $nama =$_POST['nama'];
+            $gender =$_POST['gender'];
+            $email =$_POST['email'];
+            $number =$_POST['nomor'];
+            $agama =$_POST['agama'];
+            $bahasa =$_POST['bahasa'];
+            $gambaran =$_POST['gambaran'];
+
+            if($nama && $email){
+                $fp = fopen("calon_tim.txt","a+");
+
+                fputs($fp, "\n Nama : $nama\n Jenis Kelamin : $gender\n Email : $email\n Nomor WA/HP : $number\n Agaman : $agama\n Bahasa Pemrograman : ");
+                    foreach($_POST["bahasa"] as $bahasa){
+                        fputs($fp, " | $bahasa");
+                    }
+                fputs($fp, "\n Deskripsi diri : $gambaran");
+                fclose($fp);
+
+                echo '<script language="javascript">alert("Terima kasih");</script>';
+            }else{
+                echo '<script language="javascript">alert("Mohon isi data dengan benar");</script>';
+            }
+        }
+
+
+    ?>
+</body>
+</html>
---
+++ responsi/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Pemrograman WEB : Responsi</title>
+    <link rel="stylesheet" type="text/css" href="css.css">
+</head>
+<body>
+    <h1>Selamat datang Calon anggota Tim</h1>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="daftar.php">Daftar</a></li>
+        <li><a href="biodata.php">Tentang</a></li>
+    </ul>
+
+    <h1>1700018106 - Brilian Anugra - Slot Jum'at 07.30</h1>
+    <div class="counter">
+        <?php
+                $filelain = "ini.txt";
+        
+                function counter(){
+                    global $filelain;
+                    if(file_exists($filelain)){
+                        $value = file_get_contents($filelain);
+                    }else{
+                        $value = 0;
+                    }
+                    file_put_contents($filelain,++$value);
+                }
+        
+                counter();
+                echo 'Selamat Datang<br>';
+                echo 'Anda Pengunjung Ke-'.file_get_contents($filelain),'<br>';
+                echo 'Responsi Pemrogramana WEB<br>';
+            ?>
+    </div>
+</body>
+
+</html>
